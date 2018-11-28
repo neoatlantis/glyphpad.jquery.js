4e1942b0c87ad9e846b3f8c0d4b7cfcd030084cd
Chaobai Li <contact@chaobai.li>
2018-11-28 20:02:25 +0100

    128 glyphs

--- glyphpad.jquery.js
+++ glyphpad.jquery.js
@@ -1,6 +1,137 @@
 (function($){
 //////////////////////////////////////////////////////////////////////////////
 
+const glyphtionary = {
+    "1A-AX-XC-C4-45": "abandon",
+    "3C-CX-XD": "adapt",
+    "2B-4B": "advance",
+    "1A-AX-XD-D6-61": "after",
+    "4B-BC-CX-XA-AD": "repeat",
+    "12-23-34-45-56-61": "all",
+    "AB-AD-DX": "answer",
+    "4B-B2-2A-A6": "war",
+    "32-2A-A1-1D": "avoid",
+    "2X-XD-D6": "barrier",
+    "3B-BX-XC-C4-43": "before",
+    "2C-5C-5D": "begin",
+    "5C-5D-AB-AD-BC": "human",
+    "AB-AX-BX": "body",
+    "1A-3B-AX-BX": "breath",
+    "1D-45-4C-CX-DX": "capture",
+    "5D-5X-CX": "change",
+    "12-1A-23-34-5C-AX-CX": "chaos",
+    "2X-5X": "clear",
+    "12-16-23-2X-34-45-56-5X": "clear-all",
+    "AB-BX-CX": "complex",
+    "4B-6A-AD-BC-CD": "conflict",
+    "3B-6D-BC-CD": "consequence",
+    "12-16-56-5C-AX-BC-BX": "contemplate",
+    "6A-AD": "reduce",
+    "4B-BC-CD": "courage",
+    "1A-4C-AX-CX": "create",
+    "16-1A-34-3B-4C-6D-BX-DX": "idea",
+    "2B-5X-BX": "danger",
+    "2A-5C-AX-CX": "data",
+    "1D-3C-5C-5D": "defend",
+    "16-56": "destination",
+    "5C-AD-AX-BX-CD": "destiny",
+    "3B-6D-BX-DX": "destroy",
+    "4C-BX-CX": "deteriorate",
+    "4C-6D-CX-DX": "die",
+    "1A-AD-CX-DX": "difficult",
+    "16-45-56": "discover",
+    "23-34": "outside",
+    "5C-AX-CX": "easy",
+    "12-1D-2X-5D-5X": "end",
+    "12-16-2B-56-AB-AX-BX": "enlightened",
+    "AB-AD-BC": "equal",
+    "12-1A-AB-BC": "escape",
+    "2X-BC-BX": "evolution",
+    "2X-AD-AX": "failure",
+    "1D-AB-AD": "fear",
+    "16-1A-2A": "follow",
+    "4C": "forget",
+    "1A-6D-AD": "future",
+    "3C": "gain",
+    "1A-3B-AD-BC-CD": "civilization",
+    "4B-BC": "grow",
+    "2A-2B-6D-AX-BX-DX": "harm",
+    "2A-2B-5C-5D-AX-BX-CX-DX": "harmony",
+    "5C-CX-DX": "have",
+    "3B-BX-CD-CX": "help",
+    "1A-1D-AB-CD": "hide",
+    "5A-5B-AB": "me",
+    "6D": "ignore",
+    "AC-AX-BC-BX-CX": "imperfect",
+    "1A-AX-DX": "improve",
+    "5X-BC-BX-CX": "impure",
+    "1A-4C-AX-BC-BX": "intelligence",
+    "2X-34-3B-4C-5X-BX-CX": "interrupt",
+    "1A-34-3B-45-AX-BX": "journey",
+    "5A-5B-AX-BX": "knowledge",
+    "23-34-4C-5C": "lead",
+    "12-1A-23-3B-4C-6D-AD-BC": "legacy",
+    "AX-BX": "less",
+    "12-1A-4B-AX-BX": "liberate",
+    "AD-AX-BC-BX-DX": "lie",
+    "1A-4B-AX-BC-CX": "reincarnate",
+    "1D": "loss",
+    "1D-4B-BX-DX": "message",
+    "5C-5X-BC-BX": "mind",
+    "CX-DX": "more",
+    "2A-2B-3B-AB-BC": "mystery",
+    "2A-2B-2X-5X-AX-BX": "n'zeer",
+    "4C-6D-AB-AD-BC": "nature",
+    "6D-AD": "new",
+    "AB-AD": "inside",
+    "45-4C-5X-CX": "nourish",
+    "3B-BC": "old",
+    "5C-5D-CD": "open",
+    "12-16-23-34-45-56-5C-5D-CD": "open-all",
+    "16-1A-34-3B-4C-6D-AB-CD": "portal",
+    "3B-4C-BC": "past",
+    "2X-4C-CX": "path",
+    "2X-45-4C-56-6D-CX-DX": "perfect",
+    "2A-2B-4C-6D-AX-BX-CX-DX": "perspective",
+    "16-2X-6D-DX": "potential",
+    "5C-5D-AD-AX-BC-BX-CD": "presence",
+    "AD-BC-CD": "now",
+    "2X-AD-AX-DX": "pure",
+    "2A-2B-3B": "pursue",
+    "2A-AB-BC": "question",
+    "6D-AB-BX-DX": "react",
+    "16-1A-3C-AX-CX": "rebel",
+    "23-2X-3B-BX": "repair",
+    "2B-2X-5C-5X-AB": "resistance",
+    "3B-56-6D-BX-DX": "restraint",
+    "2A-6A": "retreat",
+    "4B-6A-AB": "safety",
+    "1D-CX-DX": "save",
+    "2B": "see",
+    "AB-AX-BC-CD": "search",
+    "45-56": "self",
+    "3B-6D-AD-AX-BC-CX": "separate",
+    "2A-2B-4C-6D-AD-BC": "shaper",
+    "45-4C-6D-CD": "share",
+    "CD": "simple",
+    "5D-5X-AD-AX": "soul",
+    "4C-6D-CD": "stay",
+    "AB-AD-BC-CD": "strong",
+    "1A-6D-AX-BC-BX-CX-DX": "technology",
+    "2C-CD": "them",
+    "4C-AB-AX-BX-CX": "together",
+    "AD-AX-BC-BX-CX-DX": "truth",
+    "12-1D-23-34-45-56-AB-AX-BC-CD": "unbounded",
+    "1D-DX": "use",
+    "2A-2B-5A-5B": "victory",
+    "4C-5C-5D": "want",
+    "5A-AB": "us",
+    "3B-AB-AD": "weak",
+    "1D-3C-CX-DX": "worth",
+    "AB-AD-BC-CX-DX": "xm",
+    "2C-2D-CD": "you",
+};
+
 function svgEl(tagName, attributes) {
     var el = document.createElementNS("http://www.w3.org/2000/svg", tagName);
     if(attributes){
@@ -37,6 +168,9 @@ function initSVG(svg){
         width = svg.viewBox.baseVal.width;
     var padSize = height * 0.4, circleSize = height * 0.035;
 
+    svg.setAttribute("width", width);
+    svg.setAttribute("height", height);
+
     function fictionToRealCoord(r, theta){
         var x = r * padSize * Math.cos(theta) + width / 2,
             y = height / 2 - r * padSize * Math.sin(theta);
@@ -56,7 +190,7 @@ function initSVG(svg){
         });
 
         svg.appendChild(circle);
-        addMagicToCircle(circle);
+        //addMagicToCircle(circle);
         return circle;
     }
 
@@ -74,12 +208,11 @@ function initSVG(svg){
     drawCircle(1, SIX_DEGREES * 5.5,    "6");
 }
 
-function addMagicToCircle(circle){
-    return;
+/*function addMagicToCircle(circle){
     $(circle).on("vmouseover vmousemove mouseleave mouseenter", function(){
         $(this).trigger("mouseover_glyph", $(this).attr("data-name"));
     });
-}
+}*/
 
 
 //----------------------------------------------------------------------------
@@ -94,7 +227,7 @@ function addTracingEvents(svg){
     var circlesPosition = {};
 
     function pushPolyline(x, y){
-        if(!currentPolyline){// || currentTrace.length > 100 * 4){
+        if(!currentPolyline || currentTrace.length > 100){
             currentPolyline = svgEl("polyline", {"class": "glyphpad-trace"});
             currentTrace = "";
             svg.appendChild(currentPolyline);
@@ -154,8 +287,7 @@ function addTracingEvents(svg){
     function endTracing(){
         var evaluation = evaluateStrokes(foundStrokes);
         if(evaluation){
-            console.log(evaluation);
-            $(svg).trigger("glyph_detection", evaluation);
+            $(svg).trigger("detection", evaluation);
         }
         tracing = false;
         currentTrace = "";
@@ -206,7 +338,7 @@ function addTracingEvents(svg){
         }
     }
 
-    $(svg)
+    return $(svg)
         .on("vmousedown", startTracing)
         .on("vmouseup", endTracing)
         .on("vmousemove", traceMovement)
@@ -217,20 +349,7 @@ function addTracingEvents(svg){
 
 //----------------------------------------------------------------------------
 // Evaluate strokes
-var glyphtionary = {
-    "1A-AX-XC-C4-45": "abandon",
-    "3C-CX-XD": "adapt",
-    "2B-4B": "advance",
-    "1A-AX-XD-D6-61": "after",
-    "4B-BC-CX-XA-AD": "repeat",
-    "12-23-34-45-56-61": "all",
-    "AB-AD-DX": "answer",
-    "4B-B2-2A-A6": "war",
-    "32-2A-A1-1D": "avoid",
-    "2X-XD-D6": "barrier",
-    "3B-BX-XC-C4-43": "before",
-    "AB-AX-BX": "body",
-};
+
 var compiledGlyphtionary = {};
 for(var seq in glyphtionary){
     var seq1 = seq.split("-");
@@ -243,7 +362,6 @@ for(var seq in glyphtionary){
     seq2.sort();
     compiledGlyphtionary[seq2.join("-")] = glyphtionary[seq];
 }
-console.log(compiledGlyphtionary);
 
 
 function evaluateStrokes(strokes){
@@ -251,7 +369,6 @@ function evaluateStrokes(strokes){
     var index = strokes.join("-");
     console.log("Evaluate:", index);
     if(compiledGlyphtionary[index]){
-        alert(compiledGlyphtionary[index]);
         return compiledGlyphtionary[index];
     }
     return null;
@@ -260,9 +377,9 @@ function evaluateStrokes(strokes){
 
 
 $.fn.glyphpad = function(){
-    this.each(function(){
+    return this.each(function(){
         initSVG(this);
-        addTracingEvents(this);
+        return addTracingEvents(this);
     });
 
 };
